188cc7a963fc689096fb2730916623dd45e5d942
Bob Lutz <rjlutz@gmail.com>
2023-05-19 11:37:03 -0400

    WIP - adding CodingBat exercises

---
+++ Activities/CodingBat/List2-BigDiff.py
@@ -0,0 +1,18 @@
+# https://codingbat.com/prob/p184853 big diff
+
+# Given an array length 1 or more of ints, return the difference
+# between the largest and smallest values in the array. Note: the
+# built-in min(v1, v2) and max(v1, v2) functions return the smaller
+# or larger of two values.
+
+# big_diff([10, 3, 5, 6]) → 7
+# big_diff([7, 2, 10, 9]) → 8
+# big_diff([2, 10, 7, 2]) → 8
+def big_diff(nums):
+    largest = max(nums)   # get largest
+    smallest = min(nums)   # get lowest
+    return largest - smallest
+
+print(big_diff([10, 3, 5, 6]))
+print(big_diff([7, 2, 10, 9]))
+print(big_diff([2, 10, 7, 2]))
